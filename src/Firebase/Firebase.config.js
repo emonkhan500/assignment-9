362a666e9ea0819a4c844d1443376286a64f3f4d
emonkhan500 <khdilo47@gmail.com>
2023-10-07 09:13:24 +0600

    react,daisy,tailwind,firebase installed

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvkZvFuclQDrNZkucY0IDYsWlOkdoqmiY",
+  authDomain: "assignment-9-57b6e.firebaseapp.com",
+  projectId: "assignment-9-57b6e",
+  storageBucket: "assignment-9-57b6e.appspot.com",
+  messagingSenderId: "604891997811",
+  appId: "1:604891997811:web:48ccd7a9934b81bcdc2c06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
